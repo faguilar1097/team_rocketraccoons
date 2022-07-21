7c40e407dcec38b1b6df7b4fc68619ae8de61c91
Francisco Aguilar <f.aguilar.work97@gmail.com>
2022-07-21 10:26:40 -0500

    Updating: actualizando mi carpeta

---
+++ franciscoaguilar/html/script.js
@@ -0,0 +1,21 @@
+console.log("Conexión con js bien hecha");
+document.getElementById("MEX2727").setAttribute("fill","blue");
+document.getElementById("MEX2711").setAttribute("fill","blue");
+document.getElementById("MEX2731").setAttribute("fill","blue");
+document.getElementById("MEX2714").setAttribute("fill","blue");
+document.getElementById("MEX2711").setAttribute("fill","blue");
+document.getElementById("MEX2735").setAttribute("fill","blue");
+document.getElementById("MEX2733").setAttribute("fill","blue");
+
+
+function cambiarColor(){
+    
+    document.getElementById("MEX2727").setAttribute("fill","red");
+    document.getElementById("MEX2711").setAttribute("fill","red");
+    document.getElementById("MEX2731").setAttribute("fill","red");
+    document.getElementById("MEX2714").setAttribute("fill","red");
+    document.getElementById("MEX2711").setAttribute("fill","red");
+    document.getElementById("MEX2735").setAttribute("fill","red");
+    document.getElementById("MEX2733").setAttribute("fill","red");
+
+}
