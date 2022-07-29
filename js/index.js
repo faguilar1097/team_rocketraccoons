1be374c168dcdae34564996eec386dee02b5c67b
Francisco Aguilar <f.aguilar.work97@gmail.com>
2022-07-29 00:33:35 -0500

    Update: js con fetch funcional

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 console.log("Succes!!");
+
+function VincularJSON() {
+    fetch('./assets/users/users.json')
+  .then(responseJSON =>{ return responseJSON.json()} )
+  .then(usuarios => {
+    console.log(usuarios) 
+    for (user of usuarios){
+        console.log(`id: ${user.id } tiene email ${user.email }`)
+    }
+});
+
+}
+VincularJSON();
+
