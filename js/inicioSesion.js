5d486aa98d7cf8831a6175f4fee708dfd8c840f0
Jorge Montalvo <jorgemontalvo96@gmail.com>
2022-07-29 10:51:25 -0500

    actualizacion inicioSesion

---
+++ js/inicioSesion.js
@@ -0,0 +1,53 @@
+console.log("Succes!!");
+
+function VincularJSON() {
+    fetch('./assets/users/users.json')
+  .then(responseJSON =>{ return responseJSON.json()} )
+  .then(usuarios => {
+    console.log(usuarios) 
+    let num = 1; 
+    for (user of usuarios){
+        console.log(`id: ${user.id } tiene email ${user.email }`)
+        localStorage.setItem("Perfil "+num,JSON.stringify(user));
+        num++;
+    }
+});
+}
+VincularJSON();
+
+function obtener(){
+    let listaUsuarios = JSON.parse(localStorage.getItem('Perfil ' + i))
+
+    return listaUsuarios
+}
+
+function validacion (correo, contrasenia){
+    let listaUsuarios = obtener ();
+    let acceso = false;
+
+    for (let i = 0;i < listaUsuarios.length; i++) {
+        if (correo == listaUsuarios[i][0] && contrasenia == listaUsuarios[i][1]) console.log("ACCESO CONCEDIDO");
+        else console.log("ACCESO DENEGADO");
+
+        
+    }
+
+}
+// function login() {
+//     let user;
+//     let pass;
+
+//     user = document.getElementById("usuario").value;
+//     pass = document.getElementById("password").value;
+    
+//     if (user == user.email.value && pass == user.password.value) console.log("INICIO EXITOSO");
+//     else console.log("Acceso Denegado");
+// }
+    
+
+
+
+
+
+
+
