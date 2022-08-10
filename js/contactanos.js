472bf1c00361bd1a98f914032962cdb01ab6449a
Beyra Enriquez <enriquezbeyra@gmail.com>
2022-08-09 19:18:42 -0500

    modificacion: se agrega accordion

--- js/contactanos.js
+++ js/contactanos.js
@@ -1,15 +1,21 @@
+// Validación de formulario contactanos
+
 const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 
+
 form.addEventListener('submit', (e) => {
-    let messages = []
+  let messages = []
     if (asunto.value === '' || mensaje.value === '' ) {
       messages.push('Favor de completar los campos requeridos.')
+      console.log("No se han ingresado los datos")
+      console.log("Completa el asunto y/o mensaje")
     }
     if (messages.length > 0) {
       e.preventDefault()
       errorElement.innerText = messages.join('')
     }
-})
+  }
+)
