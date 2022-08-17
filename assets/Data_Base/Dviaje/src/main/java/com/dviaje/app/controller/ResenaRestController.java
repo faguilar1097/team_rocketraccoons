9cfeb7409482276b2a64f50decb902e2950980e8
Francisco Aguilar <f.aguilar.work97@gmail.com>
2022-08-17 00:09:24 -0500

    Fix: agregando metodo PUT en resenaRestController

--- assets/Data_Base/Dviaje/src/main/java/com/dviaje/app/controller/ResenaRestController.java
+++ assets/Data_Base/Dviaje/src/main/java/com/dviaje/app/controller/ResenaRestController.java
@@ -2,11 +2,13 @@ package com.dviaje.app.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,11 @@ public class ResenaRestController {
 		return ResenaService.saveResena(Resena);
 	}
 	
+	@PutMapping("/resena")
+	public Resena replaceResena(@RequestBody Resena Resena) {
+		//Filtros para asegurar que esté un ID y exista
+		return ResenaService.saveResena(Resena);
+	}
 	@DeleteMapping("/resena/{id}")
 	public Resena borrarResena(@PathVariable Long id){
 		return ResenaService.deleteResenaById(id);		
