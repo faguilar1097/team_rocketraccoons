2f6ce48a06db63300712ee00d0348e5c466676ed
Guadalupe Rubio <lupita10x@gmail.com>
2022-08-19 12:09:04 -0500

    publicaciones

--- js/publicaciones.js
+++ js/publicaciones.js
@@ -1,5 +1,5 @@
 console.log("probando");
-
+// 
 function captura() {
     let tituloViaje=document.getElementById("titulo").value;
     let rutaViaje=document.getElementById("ruta").value;
@@ -8,11 +8,9 @@ function captura() {
     let mejorFecha=document.getElementById("fecha").value;
     let desViaje=document.getElementById("descripcion").value;
     let tipsViajes=document.getElementById("tips").value;
-    let vipsViajes=document.getElementById("seguridad").value;
+    let segViajes=document.getElementById("seguridad").value;
     let estadoRep=document.getElementById("estado").value;
-    let image=document.getElementById("image").value;
-
-
+    // 
 console.log(tituloViaje);
 console.log(rutaViaje);
 console.log(tipoClima);
@@ -22,12 +20,11 @@ console.log(desViaje)
 console.log(tipsViajes);
 console.log(segViajes);
 console.log(estadoRep);
-console-log(image);
 }
-
+// 
 function VinJSON() {
     // fetch('./assets/users/users.json')
-    fetch('publicaciones.json')
+    fetch('publi.json')
   .then(responseJSON =>{ return responseJSON.json()} )
   .then(publicaciones => {
     console.log(publicaciones) 
@@ -35,11 +32,11 @@ function VinJSON() {
     for (publi of publicaciones){
         console.log(`titulo: ${publi.titulo } ruta:${publi.ruta}  clima: ${publi.clima}  
        catego: ${publi.catego} fecha: ${publi.fecha} descripcion: ${publi.fecha} tips: ${publi.tips} 
-        seguridad:${publi.seguridad} estado: ${publi.estado} image: ${publi.file }`)
+        seguridad:${publi.seguridad} estado: ${publi.estado}`)
         localStorage.setItem("Publicaciones "+num,JSON.stringify(publi));
         num++;
     }
+    console.log(publi);
 });
-
 
 }
